daf72c80c80b66c355212000a40a8c01549e8475
AnastasiyaShved <pav159208@gmail.om>
2023-08-21 20:42:32 +0300

    Projeckt created

---
+++ GitAppHW-13/GitAppHW-13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitAppHW-13
+//
+//  Created by Apple on 21.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
